cd7a4454e0d4a172d5c762bc32fa68549e2ae4bc
Damaris Santiago <damarissantiago@10-18-224-45.dynapool.wireless.nyu.edu>
2021-03-26 15:08:12 -0400

    Adjusted output of get_heroku_status, Damaris, Indra

--- APIServer/Heroku/pull.py
+++ APIServer/Heroku/pull.py
@@ -5,11 +5,12 @@ from APIServer.commons.api_utils import read_json
 HEROKU_CONFIG_PATH = 'Heroku/heroku_config.json'
 heroku_config = read_json(HEROKU_CONFIG_PATH)
 
+
 def get_heroku_status(text):
     URL = "https://status.heroku.com/api/v4/current-status"
-    response = requests.get(url = URL)
-    data =response.json()
+    response = requests.get(url=URL)
+    # data = response.json()
+    data = response
     print(response.status_code)
-    print(data)
-    return {response.status_code: data}
-
+    print(data.text)
+    return {response.status_code: data.text}
