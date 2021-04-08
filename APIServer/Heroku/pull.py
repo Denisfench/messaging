7f2f09b72e1572a816d1b5d578ec4e1b3993ec7e
bnf239 <bnf239@nyu.edu>
2021-04-07 22:36:24 -0400

    updated get_heroku_deployments Rabiya, Begum

--- APIServer/Heroku/pull.py
+++ APIServer/Heroku/pull.py
@@ -1,37 +1,21 @@
 import requests
-
 from APIServer.commons.api_utils import read_json
 
-# from APIServer.Heroku.operations import write_status
 
 HEROKU_CONFIG_PATH = 'Heroku/heroku_config.json'
 heroku_config = read_json(HEROKU_CONFIG_PATH)
 SLACK_CONFIG_PATH = 'slack/slack_config.json'
 slack_config = read_json(SLACK_CONFIG_PATH)
 
-TEST_HEROKU_DEPLOYS = {
-    "1": "03/31/2021",
-    "2": "04/1/2021",
-    "3": "04/2/2021",
-}
-
 
 def get_heroku_deployments(server):
     """
     We will return (all?) Heroku deployments for the app of interest.
     """
-    return TEST_HEROKU_DEPLOYS
-
-
-def get_heroku_status(text):
-    """
-    Fetch app info from Heroku API
-    """
     URL = "https://api.heroku.com/apps"
     headers = {"Accept": "application/vnd.heroku+json; version=3",
                "Authorization": heroku_config['Heroku_Token']}
     response = requests.get(url=URL, headers=headers)
-    # write_status(response.text)
     return {response.status_code: response.text}
 
 
