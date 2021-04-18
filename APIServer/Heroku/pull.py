d1d0fe6bc67afc1287df5a7783e4726afe6eb5e8
Rabiya Sharieff <rs5981@nyu.edu>
2021-04-18 12:30:19 -0400

    create email function

--- APIServer/Heroku/pull.py
+++ APIServer/Heroku/pull.py
@@ -2,6 +2,12 @@ import requests
 from APIServer.commons.api_utils import read_json
 
 
+# for dealing with attachement MIME types
+from email.mime.text import MIMEText
+
+SCOPES = ['https://mail.google.com/']
+our_email = 'rabiya.sharieff@gmail.com'  # for testing
+
 HEROKU_CONFIG_PATH = 'Heroku/heroku_config.json'
 heroku_config = read_json(HEROKU_CONFIG_PATH)
 SLACK_CONFIG_PATH = 'slack/slack_config.json'
@@ -28,6 +34,19 @@ def send_text_to_slack_channel(textToSend, channel):
                "Content-Type": "application/json; charset=utf-8"}
     response = requests.post(URL, json=textToSend, headers=headers)
     return {response.status_code: response.text}
+
+
+def create_email(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Returns:
+    An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': (message.as_string())}
 
 
 def send_email(textToSend, email):
